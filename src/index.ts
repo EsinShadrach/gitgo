3432e3272d58a9f1336e1c84ec1d1120144931d0
EsinShadrach <esinshadrachrafe@gmail.com>
2024-01-25 23:33:26 +0100

    Main function

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,23 @@ import { helpText } from "./help-text";
 
 const argurment = process.argv.slice(2);
 
-argurment.forEach(async (arg) => {
-  if (arg === "-h" || arg === "--help") {
-    console.log(helpText);
-  }
-});
+async function main() {
+  argurment.forEach(async (arg) => {
+    if (arg === "-h" || arg === "--help") {
+      console.log(helpText);
+    }
+  });
 
-const remoteUrl = $`git remote -v`.lines();
-for await (const remote of remoteUrl) {
-  if (remote.includes("fetch")) {
-    const fetchRemoteUrl = `${remote.split("\t")[1]}`;
-    const gitRemoteUrl = fetchRemoteUrl.split(" ")[0];
-    const gitRemote = new GitRemote(gitRemoteUrl);
-    await gitRemote.openRemoteUrl();
+  const remoteUrl = $`git remote -v`.lines();
+  for await (const remote of remoteUrl) {
+    if (remote.includes("fetch")) {
+      const fetchRemoteUrl = `${remote.split("\t")[1]}`;
+      const gitRemoteUrl = fetchRemoteUrl.split(" ")[0];
+      const gitRemote = new GitRemote(gitRemoteUrl);
+      await gitRemote.openRemoteUrl();
+    }
   }
 }
+
+main();
 
